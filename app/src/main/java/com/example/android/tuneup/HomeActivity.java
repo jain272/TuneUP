58112b8349cb5fdc43a1112d0c12290ef7f0235b
Jatin Jain <jain272@purdue.edu>
2018-09-16 05:17:25 -0400

    "Final Version of TuneUP, Ready to be Deployed"

--- app/src/main/java/com/example/android/tuneup/HomeActivity.java
+++ app/src/main/java/com/example/android/tuneup/HomeActivity.java
@@ -1,19 +1,101 @@
 package com.example.android.tuneup;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 public class HomeActivity extends AppCompatActivity {
 
-    int activity = 0;
+    String a, weather1;
+    double temp;
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+
+    public static final String USGS_BASE_URL = "http://dataservice.accuweather1.com/currentconditions/v1/2135952";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        @SuppressLint("StaticFieldLeak")
+        AsyncTask <Void, Void, Void> mAsyncTask = new AsyncTask <Void, Void, Void> () {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Uri uri = Uri.parse(USGS_BASE_URL);
+                Uri.Builder builder = uri.buildUpon();
+                builder.appendQueryParameter("apikey", "Jnqp30Uq1AmA4DpqGFa9vclJNBGcXXXOb");
+                builder.appendQueryParameter("q", "West Lafayette");
+                uri = builder.build();
+
+                URL url;
+                try{
+                    url = new URL(uri.toString());
+                } catch (MalformedURLException e) {
+                    url = null;
+                    e.printStackTrace();
+                }
+
+                HttpURLConnection connection;
+                try {
+                    assert url != null;
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setReadTimeout(10000);
+                    connection.setConnectTimeout(15000);
+                    connection.setRequestMethod("GET");
+                    connection.connect();
+
+                    Log.v(TAG, "Response code: " + connection.getResponseCode());
+
+                    BufferedReader reader = null;
+                    if (connection.getResponseCode() == 200){
+                        InputStream stream = connection.getInputStream();
+                        StringBuilder output = new StringBuilder();
+                        InputStreamReader inputStreamReader = new InputStreamReader(stream, Charset.forName("UTF-8"));
+                        reader = new BufferedReader(inputStreamReader);
+                        String line = reader.readLine();
+                        while (line != null){
+                            output.append(line);
+                            line = reader.readLine();
+                        }
+                        a = "" + output;
+                        try {
+                            JSONObject b = new JSONArray(a).getJSONObject(0);
+                            weather1 = b.getString("weather1Text");
+                            temp = b.getJSONObject("Temperature").getJSONObject("Imperial").getDouble("Value");
+                        } catch (JSONException e) {
+                            //Ignore
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+        mAsyncTask.execute();
     }
+
+    int activity = 0;
 
     /** Called when the user taps the Exercising button */
     public void Mood1 (View view) {
@@ -22,6 +104,8 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, PlayerActivity.class);
         intent.putExtra("Mood", "Exercising");
         intent.putExtra("activityID", activity);
+        intent.putExtra("weather1ID", weather1);
+        intent.putExtra("TempID", temp);
         startActivity(intent);
     }
 
@@ -32,6 +116,8 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, PlayerActivity.class);
         intent.putExtra("Mood", "Partying");
         intent.putExtra("activityID", activity);
+        intent.putExtra("weather1ID", weather1);
+        intent.putExtra("TempID", temp);
         startActivity(intent);
     }
 
@@ -42,6 +128,8 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, PlayerActivity.class);
         intent.putExtra("Mood", "Relaxing");
         intent.putExtra("activityID", activity);
+        intent.putExtra("weather1ID", weather1);
+        intent.putExtra("TempID", temp);
         startActivity(intent);
     }
 
@@ -52,6 +140,8 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, PlayerActivity.class);
         intent.putExtra("Mood", "Studying");
         intent.putExtra("activityID", activity);
+        intent.putExtra("weather1ID", weather1);
+        intent.putExtra("TempID", temp);
         startActivity(intent);
     }
 
@@ -62,6 +152,8 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, PlayerActivity.class);
         intent.putExtra("Mood", "Chilling");
         intent.putExtra("activityID", activity);
+        intent.putExtra("weather1ID", weather1);
+        intent.putExtra("TempID", temp);
         startActivity(intent);
     }
 
@@ -72,6 +164,8 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, PlayerActivity.class);
         intent.putExtra("Mood", "Tune-ing!");
         intent.putExtra("activityID", activity);
+        intent.putExtra("weather1ID", weather1);
+        intent.putExtra("TempID", temp);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android/tuneup/PlayerActivity.java
+++ app/src/main/java/com/example/android/tuneup/PlayerActivity.java
@@ -9,9 +9,18 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PlayerActivity extends AppCompatActivity {
 
     int activity = 0;
+    String weather1;
+    double temp;
+    int activityPoints = 50;
+    char weather;
+    int time;
+    String mood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,9 @@ public class PlayerActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String Mood = intent.getStringExtra("Mood");
         activity = intent.getIntExtra("activityID", 0);
+        Bundle bundle = intent.getExtras();
+        weather1 = bundle.getString("weather1ID","");
+        temp = bundle.getDouble("TempID");
         TextView textView = findViewById(R.id.T1);
         textView.setText("Current Mood: " + Mood);
 
@@ -29,33 +41,300 @@ public class PlayerActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 switch (activity) {
-                    case 1:
-                        SwitchSG();
+                    case 1: {
+                        if (temp <= 30) {
+                            activityPoints -= 25;
+                        } else if (temp > 30 && temp <= 50) {
+                            activityPoints -= 10;
+                        } else if (temp > 50 && temp <= 80) {
+                            activityPoints += 25;
+                        } else {
+                            activityPoints -= 10;
+                        }
+                        switch (weather) {
+                            case 'R':
+                                activityPoints -= 25;
+                                break;
+                            case 'C':
+                                activityPoints -= 10;
+                                break;
+                            case 'P':
+                                activityPoints += 25;
+                                break;
+                            case 'S':
+                                activityPoints += 10;
+                                break;
+                            default:
+                                activityPoints += 0;
+                                break;
+                        }
+                        if (time < 6) {
+                            activityPoints -= 15;
+                        } else {
+                            activityPoints += 15;
+                        }
+                        if (activityPoints < 25) {
+                            mood = "Mellow";
+                        } else if (activityPoints >= 25 && activityPoints < 50) {
+                            mood = "Semi-Mellow";
+                        } else if (activityPoints >= 50 && activityPoints < 75) {
+                            mood = "Semi-Active";
+                        } else {
+                            mood = "Active";
+                        }
+                        if (mood.equals("Mellow")){
+                            SwitchSG1();
+                        }
+                        else if(mood.equals("Semi-Mellow")){
+                            SwitchSG2();
+                        }
+                        else if(mood.equals("Semi-Active")){
+                            SwitchSG3();
+                        }
+                        else {
+                            SwitchSG4();
+                        }
                         break;
-                    case 2:
+                    }
+                    case 2: {
+                        if (activityPoints < 25) {
+                            mood = "Mellow";
+                        } else if (activityPoints >= 25 && activityPoints < 50) {
+                            mood = "Semi-Mellow";
+                        } else if (activityPoints >= 50 && activityPoints < 75) {
+                            mood = "Semi-Active";
+                        } else {
+                            mood = "Active";
+                        }
                         SwitchP();
                         break;
-                    case 3:
+                    }
+                    case 3: {
+                        if (activityPoints < 25) {
+                            mood = "Mellow";
+                        } else if (activityPoints >= 25 && activityPoints < 50) {
+                            mood = "Semi-Mellow";
+                        } else if (activityPoints >= 50 && activityPoints < 75) {
+                            mood = "Semi-Active";
+                        } else {
+                            mood = "Active";
+                        }
                         SwitchR();
                         break;
-                    case 4:
-                        SwitchST();
+                    }
+                    case 4: {
+                        if (time <= 7 && time >= 4) {
+                            activityPoints -= 25;
+                        } else if (time <= 11 && time > 7) {
+                            activityPoints -= 10;
+                        } else if (time <= 1 && time > 11) {
+                            activityPoints += 0;
+                        } else if (time < 19 && time >= 11) {
+                            activityPoints += 10;
+
+                        } else if (time < 4 || time > 19) {
+                            activityPoints += 25;
+                        }
+
+                        if (weather == 'R') {
+                            activityPoints -= 15;
+                        } else {
+                            activityPoints += 15;
+                        }
+                        if (activityPoints < 25) {
+                            mood = "Mellow";
+                        } else if (activityPoints >= 25 && activityPoints < 50) {
+                            mood = "Semi-Mellow";
+                        } else if (activityPoints >= 50 && activityPoints < 75) {
+                            mood = "Semi-Active";
+                        } else {
+                            mood = "Active";
+                        }
+
+                        if (mood.equals("Mellow")) {
+                            SwitchST1();
+                        }
+                        else if(mood.equals("Semi-Mellow")){
+                            SwitchST2();
+                        }
+                        else if(mood.equals("Semi-Active")){
+                            SwitchST3();
+                        }
+                        else {
+                            SwitchST4();
+                        }
                         break;
-                    case 5:
-                        SwitchC();
+                    }
+                    case 5: {
+                        switch (weather) {
+                            case 'R':
+                                activityPoints -= 15;
+                                break;
+                            default:
+                                activityPoints += 0;
+                                break;
+                        }
+                        if (time <= 7 && time >= 4) {
+                            activityPoints += 25;
+                        } else if (time <= 11 && time > 7) {
+                            activityPoints = +15;
+                        } else if (time <= 13 && time > 11) {
+                            activityPoints += 0;
+                        } else if (time < 19 && time >= 11) {
+                            activityPoints -= 25;
+
+                        } else if (time < 4 || time > 19) {
+                            activityPoints -= 25;
+                        }
+                        if (activityPoints <= 25) {
+                            mood = "Mellow";
+                        } else if (activityPoints > 25 && activityPoints < 50) {
+                            mood = "Semi-Mellow";
+                        } else if (activityPoints > 50 && activityPoints < 75) {
+                            mood = "Semi-Active";
+                        } else {
+                            mood = "Active";
+                        }
+
+                        if (mood.equals("Mellow")) {
+                            SwitchC1();
+                        }
+                        else if(mood.equals("Semi-Mellow")){
+                            SwitchC2();
+                        }
+                        else if(mood.equals("Semi-Active")){
+                            SwitchC3();
+                        }
+                        else {
+                            SwitchC4();
+                        }
                         break;
-                    case 6:
-                        SwitchT();
+                    }
+                    case 6: {
+                        if (weather == 'R') {
+                            activityPoints -= 25;
+
+                        } else if (weather == 'S') {
+                            activityPoints += 25;
+                        } else if (weather == 'C') {
+                            activityPoints -= 10;
+                        } else if (weather == 'P') {
+                            activityPoints += 15;
+                        }
+                        if (temp < 30) {
+                            activityPoints -= 25;
+                        } else if (temp > 30 && temp <= 50) {
+                            activityPoints += 15;
+                        } else if (temp > 50 && temp <= 80) {
+                            activityPoints -= 10;
+                        } else {
+                            activityPoints += 25;
+                        }
+                        if (time >= 4 || time < 7) {
+                            activityPoints += 10;
+                        } else if (time >= 7 || time < 11) {
+                            activityPoints += 5;
+                        } else if (time >= 11 && time < 14) {
+                            activityPoints += 0;
+                        } else if (time >= 14 && time < 19) {
+                            activityPoints -= 15;
+
+                        } else if (time >= 19 || time < 4) {
+                            activityPoints -= 25;
+                        }
+                        if (activityPoints < 12.5) {
+                            mood = "Sad af";
+                        } else if (activityPoints >= 12.5 && activityPoints < 25) {
+                            mood = "Sad";
+                        } else if (activityPoints >= 25 && activityPoints < 37.5) {
+                            mood = "Mellow";
+                        } else if (activityPoints >= 37.5 && activityPoints < 40) {
+                            mood = "Semi-Mellow";
+                        } else if (activityPoints >= 40 && activityPoints < 52.5) {
+                            mood = "Normal";
+                        } else if (activityPoints >= 52.5 && activityPoints < 65) {
+                            mood = "Semi-Active";
+                        } else if (activityPoints >= 65 && activityPoints < 77.5) {
+                            mood = "Active";
+                        } else if (activityPoints >= 77.5 && activityPoints < 90) {
+                            mood = "Excited";
+                        } else {
+                            mood = "Excited af";
+                        }
+
+                        if (mood.equals("Sad af")) {
+                            SwitchT1();
+                        }
+                        else if(mood.equals("Sad")){
+                            SwitchT2();
+                        }
+                        else if(mood.equals("Mellow")){
+                            SwitchT3();
+                        }
+                        else if(mood.equals("Semi-Mellow")){
+                            SwitchT4();
+                        }
+                        else if(mood.equals("Normal")){
+                            SwitchT5();
+                        }
+                        else if(mood.equals("Semi-Active")){
+                            SwitchT6();
+                        }
+                        else if(mood.equals("Active")){
+                            SwitchT7();
+                        }
+                        else if(mood.equals("Excited")){
+                            SwitchT8();
+                        }
+                        else {
+                            SwitchT8();
+                        }
                         break;
+                    }
                     default:
+                        Default();
                 }
             }
         });
+
+        if (weather1.equals("Sunny") || weather1.equals("Mostly Sunny") || weather1.equals("Partly Sunny") || weather1.equals("Clear") || weather1.equals("Mostly Clear") || weather1.equals("Hot")) {
+            weather = 'S';
+        } else if (weather1.equals("Intermittent Clouds") || weather1.equals("Mostly Cloudy") || weather1.equals("Partly Cloudy")) {
+            weather = 'P';
+        } else if (weather1.equals("Cloudy") || weather1.equals("Dreary (Overcast)") || weather1.equals("Fog") || weather1.equals("Hazy Moonlight")) {
+            weather = 'C';
+        } else {
+            weather = 'R';
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat("HH");
+        Date date = new Date();
+        time = Integer.parseInt(formatter.format(date));
     }
 
-    public void SwitchSG() {
-        // Sports/Gym Playlist
-        Uri webpage = Uri.parse("");
+    public void SwitchSG1() {
+        //Sports/Gym Playlist1
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/1OX1q67JTZXWUdKF0oKi7e");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchSG2() {
+        //Sports/Gym Playlist2
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/3WK9bqjUmjYKZjpgOePsy8");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchSG3() {
+        //Sports/Gym Playlist3
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/4NRrIjOcwLOITmEP2HgwbP");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchSG4() {
+        //Sports/Gym Playlist4
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/1YJAlGGP8cDzqZJsoEQ3gD");
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         startActivity(intent);
     }
@@ -74,23 +353,114 @@ public class PlayerActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void SwitchST() {
-        //Studying Playlist
-        Uri webpage = Uri.parse("");
+    public void SwitchST1() {
+        //Studying Playlist1
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/2jArGkAqIWi2Sx8Hypa31p");
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         startActivity(intent);
     }
 
-    public void SwitchC() {
-        //Chilling Playlist
-        Uri webpage = Uri.parse("");
+    public void SwitchST2() {
+        //Studying Playlist2
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/7L5cshmVSw9v0X0mZIn78U");
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         startActivity(intent);
     }
 
-    public void SwitchT() {
-        //Tune Me UP Playlist
-        Uri webpage = Uri.parse("");
+    public void SwitchST3() {
+        //Studying Playlist3
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/37nHJKXeTOgQhwZXvRtGfm");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchST4() {
+        //Studying Playlist4
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/2amhJhphcPln6Ybsjh6IYH");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchC1() {
+        //Chilling Playlist1
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/0SUEaZOIRWBJsl8wWDwQAM");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchC2() {
+        //Chilling Playlist2
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/3tRCQgeVW3I9Nn47lvilrw");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchC3() {
+        //Chilling Playlist3
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/28iMFWE1FJmAmXZOxruBlZ");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchC4() {
+        //Chilling Playlist4
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/2QYuI6SROeJtqRWNxrUMxH");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchT1() {
+        //Tune Me UP Playlist1
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/2AjlccWiGYG0jvvOnC5zB7");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchT2() {
+        //Tune Me UP Playlist2
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/79gnLfvaKxD8GifGZPLx2v");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchT3() {
+        //Tune Me UP Playlist3
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/3683pKHDNgefBamcxpqs8h");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchT4() {
+        //Tune Me UP Playlist4
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/6hMiy1yc7aQwhIBkEPy579");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchT5() {
+        //Tune Me UP Playlist5
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/1uajGvEjcv1V6OU9X7P7Yf");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchT6() {
+        //Tune Me UP Playlist6
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/6jWhlLBvM2d4IDkADxZbQJ");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchT7() {
+        //Tune Me UP Playlist7
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/33JEvnoeXghLrt18qlAwcL");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void SwitchT8() {
+        //Tune Me UP Playlist8
+        Uri webpage = Uri.parse("https://open.spotify.com/playlist/0aUxW2AHypBMz8VH19FDQx");
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         startActivity(intent);
     }
